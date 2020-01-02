6ba421f6e1b6ea41c900def4915105d176042867
Yam <yam@linnovate.net>
2020-01-02 15:36:52 +0200

    Added intensive task for simulate cpu-load

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from subprocess import check_output
 
 app = Flask(__name__)
 
@@ -8,7 +9,8 @@ def hello():
     return "Hello world!"
 
 @app.route("/intense")
-def intense():
+def get_shell_script_output_using_check_output():
+    stdout = check_output(['./stress.sh']).decode('utf-8')
     return "This is intense page"
 
 
