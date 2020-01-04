d709f7554b606b0b33359936382411c9938b2378
Yam <yam@linnovate.net>
2020-01-04 21:19:43 +0200

    version 1.0.3

--- bump.sh
+++ bump.sh
@@ -15,7 +15,6 @@ docker build -t $USERNAME/$IMAGE:latest .
 git add -A
 git commit -m "version $version"
 git tag -a "$version" -m "version $version"
-git push
 git push --tags
 docker tag $HOSTNAME/$USERNAME/$IMAGE:latest $HOSTNAME/$USERNAME/$IMAGE:$version
 # push it
